8ce34278ca437327622dfc4d1cdddfe2edfc5cb8
NestorDanielGomez <87837253+NestorDanielGomez@users.noreply.github.com>
2022-07-21 11:51:47 -0300

    Correcciones menores

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,54 +1,54 @@
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { UserModel } from '../models/user';
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { UserModel } from "../models/user";
 import {
   validateNewUser,
   getUserByEmail,
   createUser,
-} from '../controllers/users';
-import Logger from './logger';
-import { NotificationService } from './notifications';
+} from "../controllers/users";
+import Logger from "./logger";
+import { NotificationService } from "./notifications";
 
 const strategyOptions = {
-  usernameField: 'email',
-  passwordField: 'password',
+  usernameField: "email",
+  passwordField: "password",
   passReqToCallback: true,
 };
 
 const login = async (req, username, password, done) => {
-  Logger.info('LOGINNN');
+  Logger.info("LOGINNN");
 
   const user = await getUserByEmail(username);
   if (!user) {
-    return done(null, false, { message: 'Invalid Username/Password' });
+    return done(null, false, { message: "Invalid Username/Password" });
   }
   const isValidPassword = await user.isValidPassword(password);
 
   if (!isValidPassword) {
-    return done(null, false, { message: 'Invalid Username/Password' });
+    return done(null, false, { message: "Invalid Username/Password" });
   }
 
-  Logger.info('SALIO TODO BIEN');
+  Logger.info("SALIO TODO BIEN");
   return done(null, user);
 };
 
 const signup = async (req, username, password, done) => {
   try {
-    Logger.info('ENTRE');
+    Logger.info("ENTRE");
     Logger.info(req.body);
     const { firstName, lastName, age, admin, address } = req.body;
 
     // Nota: Username y password no se verifica porque ya lo hace passport.
     if (validateNewUser(req.body)) {
-      Logger.error('Invalid body fields');
-      return done(null, false, { message: 'Invalid Body Fields' });
+      Logger.error("Invalid body fields");
+      return done(null, false, { message: "Invalid Body Fields" });
     }
 
     const user = await getUserByEmail(username);
 
     if (user) {
-      Logger.error('User already exists');
-      return done(null, false, { message: 'User already exists' });
+      Logger.error("El usuario ya existe");
+      return done(null, false, { message: "El usuario ya existe" });
     } else {
       const userData = {
         email: username,
@@ -80,7 +80,7 @@ export const signUpFunc = new LocalStrategy(strategyOptions, signup);
  * En este caso estamos creando una key llamado user con la info del usuario dentro de req.session.passport
  */
 passport.serializeUser((user, done) => {
-  Logger.info('Se Ejecuta el serializeUser');
+  Logger.info("Se Ejecuta el serializeUser");
   //Notar que vamos a guardar en req.session.passport el id del usuario. nada mas
   done(null, user._id);
 });
@@ -89,8 +89,8 @@ passport.serializeUser((user, done) => {
  * DeserializeUser Permite tomar la info que mandamos con el serializeUser para crear el objeto req.user
  */
 passport.deserializeUser((userId, done) => {
-  console.log('HOLAAAA\n\n\n\n\n');
-  Logger.info('Se Ejecuta el desserializeUser');
+  console.log("HOLAAAA\n\n\n\n\n");
+  Logger.info("Se Ejecuta el desserializeUser");
   //Notar que recibimos el userId en la funcion (que es lo que mandamos en el done del serializedUser)
   //Buscamos el usuario con ese id y lo retornamos. El resultado va a estar en req.user
   UserModel.findById(userId).then((user) => {
